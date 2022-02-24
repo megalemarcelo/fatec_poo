76305e86328e886855d540c27cad59b11852f41b
Marcelo <marcelo.megale@fatec.sp.gov.br>
2022-02-24 14:42:48 -0300

    ex5 concluido

---
+++ ex05.java
@@ -0,0 +1,22 @@
+//05) Ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo.
+import javax.swing.JOptionPane;
+public class ex05 {
+        public static void main(String[] args) {
+        
+        //declaração de variáveis
+        int b, h, a
+
+        //entrada de dados
+        n = Integer.parseInt(JOptionPane.showInputDialog("Digite um número"));
+
+        //processamento
+        ant = n - 1;
+        
+        //saída de dados
+        String s = String.format(
+            "O antecessor de %s é %s.",
+            n, ant
+        );
+        JOptionPane.showMessageDialog(null, s);
+    }
+}
